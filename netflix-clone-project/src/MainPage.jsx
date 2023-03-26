039b9eb940995b08b65ee6d30dbd6bbcfdf1d20e
zahra <hedyezah@gmail.com>
2023-03-26 23:43:03 +0330

    making the first card

--- netflix-clone-project/src/MainPage.jsx
+++ netflix-clone-project/src/MainPage.jsx
@@ -9,7 +9,7 @@ function MainPage(){
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fillRule="evenodd" clipRule="evenodd" d="M7.29297 19.2928L14.5859 12L7.29297 4.70706L8.70718 3.29285L16.7072 11.2928C16.8947 11.4804 17.0001 11.7347 17.0001 12C17.0001 12.2652 16.8947 12.5195 16.7072 12.7071L8.70718 20.7071L7.29297 19.2928Z" fill="currentColor"></path></svg>
             </a>
         </div>
-       <header className="mp-header bg-black d-flex align-items-center">
+       <header className="mp-header d-flex align-items-center">
             <div className="icon-wrapper">
                 <svg className="svg-default" fill="#e50914" viewBox="0 0 111 30" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img">
                     <g>
@@ -18,12 +18,12 @@ function MainPage(){
                     </g>
                 </svg>
             </div>
-            <div className="dropdown ms-auto me-4">                
+            <div className="lang-dd-wrapper dropdown ms-auto me-4">                
                 <a className="lang-dd px-2 d-flex align-items-center btn text-white py-1 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <div className="lang-icon-wrapper">
                         <svg className="svg-default" width="16" height="16" viewBox="0 0 16 16" fill="#000" xmlns="http://www.w3.org/2000/svg" data-name="Globe"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 14.5C8.23033 14.5 8.84266 14.2743 9.48679 12.986C9.76293 12.4337 9.99973 11.7621 10.1748 11H5.8252C6.00027 11.7621 6.23707 12.4337 6.51321 12.986C7.15734 14.2743 7.76967 14.5 8 14.5ZM5.57762 9.5C5.52716 9.02077 5.5 8.51911 5.5 8C5.5 7.48089 5.52716 6.97923 5.57762 6.5H10.4224C10.4728 6.97923 10.5 7.48089 10.5 8C10.5 8.51911 10.4728 9.02077 10.4224 9.5H5.57762ZM11.7092 11C11.4822 12.1217 11.1317 13.117 10.6914 13.9184C12.0137 13.3161 13.0987 12.2837 13.7678 11H11.7092ZM14.3261 9.5H11.9298C11.9759 9.01412 12 8.51269 12 8C12 7.48731 11.9759 6.98588 11.9298 6.5H14.3261C14.4398 6.98152 14.5 7.48373 14.5 8C14.5 8.51627 14.4398 9.01848 14.3261 9.5ZM4.0702 9.5H1.67393C1.56019 9.01848 1.5 8.51627 1.5 8C1.5 7.48373 1.56019 6.98152 1.67393 6.5H4.0702C4.02411 6.98588 4 7.48731 4 8C4 8.51269 4.02411 9.01412 4.0702 9.5ZM2.23221 11H4.29076C4.51779 12.1217 4.86832 13.117 5.30864 13.9184C3.98635 13.3161 2.90128 12.2837 2.23221 11ZM5.8252 5H10.1748C9.99973 4.23793 9.76293 3.56626 9.48679 3.01397C8.84266 1.72571 8.23033 1.5 8 1.5C7.76967 1.5 7.15734 1.72571 6.51321 3.01397C6.23707 3.56626 6.00027 4.23793 5.8252 5ZM11.7092 5H13.7678C13.0987 3.71627 12.0137 2.68389 10.6914 2.08162C11.1317 2.88302 11.4822 3.8783 11.7092 5ZM5.30864 2.08162C4.86832 2.88302 4.51779 3.8783 4.29076 5H2.23221C2.90128 3.71628 3.98635 2.68389 5.30864 2.08162ZM8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0Z" fill="currentColor"></path></svg>
                     </div>
-                    English
+                    <span className="dd-sel-lang">English</span>
                 </a>
                 <ul className="dropdown-menu">
                     <li><a className="dropdown-item" href="#">English</a></li>
@@ -32,8 +32,30 @@ function MainPage(){
             </div>
             <button className="si-btn px-3 btn btn-secondary fw-semibold ">Sign In</button>
        </header>
-       <main>
-
+       <main className="w-100">
+            <div className="">
+                <div className="bg-img-wrapper position-absolute top-0 start-0 w-100">
+                    <img className="bg-img position-relative w-100" src="./background_pic.jpg"/>
+                </div>
+                <div className="first-card-content w-100 d-flex flex-column align-items-center text-center text-white">
+                    <h1 className="fw-bolder" style={{"font-size": "3rem"}}>Unlimited movies, TV shows, and more.</h1>
+                    <p className="mt-2 fw-semibold" style={{"font-size": "1.5rem"}}>Watch anywhere. Cancel anytime.</p>
+                    <div className="mt-2">
+                        <form method="post">
+                            <h3 className="fs-5 fw-semibold">
+                            Ready to watch? Enter your email to create or restart your membership.
+                            </h3>
+                            <div className="d-sm-flex justify-content-center mt-3">
+                                <div className="form-floating" style={{"width": "375px"}}>
+                                    <input ctype="email" className="form-control" style={{"background-color": "rgb(0,0,0,0.5)"}} id="emailInput" placeholder="Email address"/>
+                                    <label for="emailInput">Email address</label>
+                                </div>  
+                                <button className="gs-btn btn btn-secondary d-block fs-5 fw-bolder ms-2">Get Started</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
        </main>
        <footer></footer>
     </div>
